7cf480184de7da208ee6b089c8a87e3cb479ad88
toubou91 <thodoris.bais@gmail.com>
2016-07-16 12:22:55 +0200

    Fix failing tests :no_bell:

--- src/main/java/com/cloudwise/RomanNumerals.java
+++ src/main/java/com/cloudwise/RomanNumerals.java
@@ -1,9 +1,35 @@
 package com.cloudwise;
 
+import java.util.TreeMap;
+
 public class RomanNumerals {
 
-	public String convert(int input) {
-		return "not implemented";
-	}
-	
+    private static final TreeMap<Integer, String> ROMAN_NUMERALS = new TreeMap<Integer, String>();
+
+    static {
+
+        ROMAN_NUMERALS.put(1000, "m");
+        ROMAN_NUMERALS.put(900, "cm");
+        ROMAN_NUMERALS.put(500, "d");
+        ROMAN_NUMERALS.put(400, "cd");
+        ROMAN_NUMERALS.put(100, "c");
+        ROMAN_NUMERALS.put(90, "xc");
+        ROMAN_NUMERALS.put(50, "l");
+        ROMAN_NUMERALS.put(40, "xl");
+        ROMAN_NUMERALS.put(10, "x");
+        ROMAN_NUMERALS.put(9, "ix");
+        ROMAN_NUMERALS.put(5, "v");
+        ROMAN_NUMERALS.put(4, "iv");
+        ROMAN_NUMERALS.put(1, "i");
+
+    }
+
+    public String convert(int input) {
+        int l = ROMAN_NUMERALS.floorKey(input);
+        if (input == l) {
+            return ROMAN_NUMERALS.get(input);
+        }
+        return ROMAN_NUMERALS.get(l) + convert(input - l);
+    }
+
 }
